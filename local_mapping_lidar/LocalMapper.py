b27ef551c57d79f307638a724ecf9207b05a08ff
Carlos Tojal <carlos.c.tojal@gmail.com>
2023-12-06 17:20:07 +0000

    publish the cone array and cone markers

--- local_mapping_lidar/LocalMapper.py
+++ local_mapping_lidar/LocalMapper.py
@@ -5,6 +5,8 @@ from sensor_msgs.msg import PointCloud2
 from visualization_msgs.msg import Marker, MarkerArray
 from lart_msgs.msg import Cone, ConeArray
 
+from tf2_ros import TransformListener, Buffer, TransformStamped, TransformException
+
 import open3d as o3d
 import numpy as np
 
@@ -22,15 +24,52 @@ class LocalMapper(Node):
         # marker publisher
         self.marker_pub = self.create_publisher(MarkerArray, '/markers', 10)
 
+        # create the transform buffer
+        self.tf_buffer = Buffer()
+
+        # create the transform listener
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
 
     def pcd_callback(self, msg):
-        # TODO: convert ros point cloud to open3d point cloud
-        # TODO: transform the point cloud to the base_link frame
-        # TODO: remove ground plane using RANSAC
-        # TODO: create clusters using DBSCAN
-        # TODO: publish the center of each cluster as a marker and as a cone
 
-        print("TODO: received a point cloud message")
+        # convert ros point cloud to open3d point cloud
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(np.array(msg.data, dtype=np.float32).reshape(-1, 3))
+
+        # transform the point cloud to the base_link frame
+        # lookup the transform
+        try:
+            transform = self.tf_buffer.lookup_transform('base_link', msg.header.frame_id, msg.header.stamp)
+        except TransformException as e:
+            self.get_logger().warn(e)
+            return
+        
+        # convert the transform to a numpy array
+        transform_matrix = np.array([[transform.transform.rotation.w, transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z],
+                                    [transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z, 1.0]])
+        
+        # transform the point cloud
+        pcd.transform(transform_matrix)
+
+        # remove ground plane using RANSAC
+        pcd = self.remove_ground_plane(pcd)
+
+        # cluster the cones using DBSCAN
+        cone_coords = self.cluster_cones(pcd)
+
+        # create the ConeArray message
+        cone_array = self.to_cone_array(cone_coords)
+
+        # create the MarkerArray message
+        marker_array = self.to_marker_array(cone_coords)
+
+        # publish the cone array message
+        self.cone_pub.publish(cone_array)
+
+        # publish the marker array message
+        self.marker_pub.publish(marker_array)
+
 
     def remove_ground_plane(self, pcd: o3d.geometry.PointCloud) -> o3d.geometry.PointCloud:
 
@@ -63,6 +102,54 @@ class LocalMapper(Node):
             cluster_centers.append(cluster_center)
 
         return cluster_centers
+    
+    def to_cone_array(self, cluster_centers: list) -> ConeArray:
+
+        # create the ConeArray message
+        cone_array = ConeArray()
+
+        # for each cluster center
+        for cluster_center in cluster_centers:
+            # create a cone message
+            cone = Cone()
+            # set the cone message fields
+            cone.x = cluster_center[0]
+            cone.y = cluster_center[1]
+            cone.z = cluster_center[2]
+            # append the cone message to the cone array message
+            cone_array.cones.append(cone)
+
+        return cone_array
+    
+    def to_marker_array(self, cluster_centers: list) -> MarkerArray:
+            
+        # create the MarkerArray message
+        marker_array = MarkerArray()
+
+        # for each cluster center
+        for cluster_center in cluster_centers:
+            # create a marker message
+            marker = Marker()
+            # set the marker message fields
+            marker.header.frame_id = 'base_link'
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.type = Marker.CYLINDER
+            marker.action = Marker.ADD
+            marker.pose.position.x = cluster_center[0]
+            marker.pose.position.y = cluster_center[1]
+            marker.pose.position.z = cluster_center[2]
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            # append the marker message to the marker array message
+            marker_array.markers.append(marker)
+
+        return marker_array
 
 
 
